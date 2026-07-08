65b6cc97b17d640d6b21833dcd8ce7cd8326e2e3
rishabh954 <130352206+rishabh954@users.noreply.github.com>
2026-07-08 10:30:06 +0530

    chore(repo): Clean up repo root scripts and move maintenance scripts

--- script.py
+++
@@ -1,8 +0,0 @@
-import os
-import django
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-django.setup()
-
-from apps.analytics.models import CustomReport
-CustomReport.objects.all().delete()
