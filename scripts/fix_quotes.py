880783f3687a1e7a47d71d02d229b0c989d3daa8
rishabh954 <130352206+rishabh954@users.noreply.github.com>
2026-07-11 16:59:33 +0530

    chore: clean up remaining stray scripts

--- scripts/fix_quotes.py
+++
@@ -1,9 +0,0 @@
-path = 'apps/company/views.py'
-with open(path, 'r', encoding='utf-8') as f:
-    content = f.read()
-
-content = content.replace('\\"', '"')
-
-with open(path, 'w', encoding='utf-8') as f:
-    f.write(content)
-print("Quotes fixed")
